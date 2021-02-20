e940bfbd9f55a160d5050074ca5f5f5409db1029
Sherry Li <sherryhli@users.noreply.github.com>
2021-02-20 01:14:23 -0500

    Define folder structure and setup TypeScript backend

---
+++ backend/typescript/models/person.pgmodel.ts
@@ -0,0 +1,10 @@
+import { Column, Model, Table } from "sequelize-typescript";
+
+@Table({ tableName: "people" })
+export default class Person extends Model {
+  @Column
+  name!: string
+
+  @Column
+  email!: string
+}
