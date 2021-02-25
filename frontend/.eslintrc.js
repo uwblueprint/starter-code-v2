6606fd9b8f12c2c43dcdf8cd41685c1fd8bea568
Sherry Li <sherryhli@users.noreply.github.com>
2021-02-25 15:36:36 -0500

    Configure ESLint and Prettier for frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: "./tsconfig.json",
+    createDefaultProgram: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: ["airbnb-typescript", "prettier", "plugin:prettier/recommended"],
+  rules: {},
+};
