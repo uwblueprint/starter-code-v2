a6b44e945576cba0e7e6a4cb1ccdf63860624577
raveen-singh <emailraveensingh@gmail.com>
2021-03-11 21:26:48 -0500

    cleaned up

--- backend/typescript/server.ts
+++ backend/typescript/server.ts
@@ -2,8 +2,9 @@ import cors from "cors";
 import express from "express";
 
 import { mongo, sequelize } from "./models/index";
-import Entity, { IEntity } from "./models/entity.mgmodel"
+import { Entity, IEntity } from "./models/entity.mgmodel"
 import EntityService from "./services/EntityService";
+import { addOptions } from "sequelize-typescript";
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -11,8 +12,16 @@ app.use(express.json());
 app.get("/", async (_req, res) => {
   // await MgPerson.create({ name: "First Last", email: "first@last.com" });
   // await Entity.create({ string_field: "test", int_field: 7, enum_field: 'A', string_array_field: ["test"], bool_field: false })
-  const test = new EntityService();
-  const results = await test.getEntities();
+  const entService = new EntityService();
+  // const entity: IEntity = await Entity.create({
+  //   string_field: 'test',
+  //   int_field: 7,
+  //   enum_field: 'B',
+  //   string_array_field: ['A'],
+  //   bool_field: false,
+
+  // })
+  const results = await entService.getEntities();
   res.send(results);
 });
 
