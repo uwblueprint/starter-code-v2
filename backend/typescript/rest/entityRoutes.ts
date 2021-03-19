cf82d8b39f2bfa94f87e81c593c908d60e609ceb
raveen-singh <emailraveensingh@gmail.com>
2021-03-18 22:27:27 -0400

    last few changes

--- backend/typescript/rest/entityRoutes.ts
+++ backend/typescript/rest/entityRoutes.ts
@@ -7,19 +7,11 @@ export const entityRouter: Router = Router();
 const entService = new EntityService();
 
 /* Create entity Object */
-entityRouter.post("/", async (_req, res) => {
-    const entityObj: EntityRequestDTO = _req.body.entity;
-    const IentityObj = {} as Entity;
-    const entService = new EntityService();
-    IentityObj.stringField = entityObj.stringField;
-    IentityObj.intField = entityObj.intField;
-    IentityObj.enumField = entityObj.enumField;
-    IentityObj.stringArrayField = entityObj.stringArrayField;
-    IentityObj.boolField = entityObj.boolField;
-
+entityRouter.post("/", async (req, res) => {
+    const entityObj: EntityRequestDTO = req.body.entity;
     try {
-        await entService.createEntity(IentityObj);
-        res.send('Success')
+        const newEntity = await entService.createEntity(entityObj);
+        res.status(200).json(newEntity);
     }
     catch (e) {
         res.status(500).send(e.message);
@@ -28,10 +20,10 @@ entityRouter.post("/", async (_req, res) => {
 
 
 /* Get all entity objects */
-entityRouter.get("/", async (_req, res) => {
+entityRouter.get("/", async (req, res) => {
     try {
-        const result = await entService.getEntities();
-        res.send(result)
+        const entities = await entService.getEntities();
+        res.status(200).json(entities);
     }
     catch (e) {
         res.status(500).send(e.message);
@@ -39,12 +31,12 @@ entityRouter.get("/", async (_req, res) => {
 });
 
 /* Get entity object by id */
-entityRouter.get("/:id", async (_req, res) => {
-    const id: string = _req.params.id;
+entityRouter.get("/:id", async (req, res) => {
+    const id: string = req.params.id;
 
     try {
-        const result = await entService.getEntity(id);
-        res.send(result)
+        const entity = await entService.getEntity(id);
+        res.status(200).json(entity);
     }
     catch (e) {
         res.status(500).send(e.message);
@@ -52,19 +44,12 @@ entityRouter.get("/:id", async (_req, res) => {
 });
 
 /* Update entity object by id */
-entityRouter.put("/:id", async (_req, res) => {
-    const id: string = _req.params.id;
-    const entityObj: EntityRequestDTO = _req.body.entity;
-
-    const IentityObj = {} as Entity;
-    IentityObj.stringField = entityObj.stringField;
-    IentityObj.intField = entityObj.intField;
-    IentityObj.enumField = entityObj.enumField;
-    IentityObj.stringArrayField = entityObj.stringArrayField;
-    IentityObj.boolField = entityObj.boolField;
+entityRouter.put("/:id", async (req, res) => {
+    const id: string = req.params.id;
+    const entityObj: EntityRequestDTO = req.body.entity;
     try {
-        const result = await entService.updateEntity(id, IentityObj);
-        res.send(result)
+        const entity = await entService.updateEntity(id, entityObj);
+        res.status(200).json(entity);
     }
     catch (e) {
         res.status(500).send(e.message);
@@ -72,12 +57,12 @@ entityRouter.put("/:id", async (_req, res) => {
 });
 
 /* Delete entity object by id */
-entityRouter.delete("/:id", async (_req, res) => {
-    const id: string = _req.params.id;
+entityRouter.delete("/:id", async (req, res) => {
+    const id: string = req.params.id;
 
     try {
         entService.deleteEntity(id);
-        res.send('Success')
+        res.send('Success');
     }
     catch (e) {
         res.status(500).send(e.message);
