ef2e6ce7588b5b966f65698788d66ca2c6481e8d
Sherry Li <sherryhli@users.noreply.github.com>
2021-03-29 17:51:51 -0400

    Create user REST endpoints

---
+++ backend/typescript/rest/userRoutes.ts
@@ -0,0 +1,137 @@
+import { Router } from "express";
+
+import UserService from "../services/implementations/userService";
+import IUserService from "../services/interfaces/userService";
+
+const userRouter: Router = Router();
+const userService: IUserService = new UserService();
+
+/* Get all users, optionally filter by a userId or email query parameter to retrieve a single user */
+userRouter.get("/", async (req, res) => {
+  const { userId, email } = req.query;
+
+  if (userId && email) {
+    res.status(400).json({ error: "Cannot query by both userId and email." });
+    return;
+  }
+
+  if (!userId && !email) {
+    try {
+      const users = await userService.getUsers();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+    return;
+  }
+
+  if (userId) {
+    if (typeof userId !== "string") {
+      res
+        .status(400)
+        .json({ error: "userId query parameter must be a string." });
+    } else {
+      try {
+        const user = await userService.getUserById(userId);
+        res.status(200).json(user);
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    }
+    return;
+  }
+
+  if (email) {
+    if (typeof email !== "string") {
+      res
+        .status(400)
+        .json({ error: "email query parameter must be a string." });
+    } else {
+      try {
+        const user = await userService.getUserByEmail(email);
+        res.status(200).json(user);
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    }
+  }
+});
+
+/* Create a user */
+userRouter.post("/", async (req, res) => {
+  try {
+    const newUser = await userService.createUser({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      role: req.body.role,
+      password: req.body.password,
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+/* Update the user with the specified userId */
+userRouter.put("/:userId", async (req, res) => {
+  try {
+    const updatedUser = await userService.updateUserById(req.params.userId, {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      role: req.body.role,
+    });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+/* Delete a user by userId or email, specified through a query parameter */
+userRouter.delete("/", async (req, res) => {
+  const { userId, email } = req.query;
+
+  if (userId && email) {
+    res.status(400).json({ error: "Cannot delete by both userId and email." });
+    return;
+  }
+
+  if (userId) {
+    if (typeof userId !== "string") {
+      res
+        .status(400)
+        .json({ error: "userId query parameter must be a string." });
+    } else {
+      try {
+        await userService.deleteUserById(userId);
+        res.status(204).send();
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    }
+    return;
+  }
+
+  if (email) {
+    if (typeof email !== "string") {
+      res
+        .status(400)
+        .json({ error: "email query parameter must be a string." });
+    } else {
+      try {
+        await userService.deleteUserByEmail(email);
+        res.status(204).send();
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    }
+    return;
+  }
+
+  res
+    .status(400)
+    .json({ error: "Must supply one of userId or email as query parameter." });
+});
+
+export default userRouter;
--- backend/typescript/server.ts
+++ backend/typescript/server.ts
@@ -4,13 +4,7 @@ import * as firebaseAdmin from "firebase-admin";
 
 import { mongo, sequelize } from "./models";
 import entityRouter from "./rest/entityRoutes";
-
-import UserService from "./services/implementations/userService";
-import IUserService from "./services/interfaces/userService";
-
-import AuthService from "./services/implementations/authService";
-import IAuthService from "./services/interfaces/authService";
-import { Role } from "./types";
+import userRouter from "./rest/userRoutes";
 
 const app = express();
 app.use(cors());
@@ -18,95 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use("/entities", entityRouter);
-const userService: IUserService = new UserService();
-
-/** ***************************************************************************
- * TEMPORARY: UserService and AuthService test endpoints
- *************************************************************************** */
-app.get("/create-user/:firstName/:lastName", async (req, res) => {
-  const newUser = await userService.createUser({
-    firstName: req.params.firstName,
-    lastName: req.params.lastName,
-    email: `${req.params.firstName}@${req.params.lastName}.com`,
-    role: "User",
-    password: "s3cur3p455w0rd",
-  });
-  res.status(200).json(newUser);
-});
-
-app.get("/update-user/:userId", async (req, res) => {
-  const updatedUser = await userService.updateUserById(req.params.userId, {
-    firstName: "Updated",
-    lastName: "User",
-    email: "updated@user.org",
-    role: "User",
-  });
-  res.status(200).json(updatedUser);
-});
-
-app.get("/get-user-by-id/:userId", async (req, res) => {
-  const user = await userService.getUserById(req.params.userId);
-  res.status(200).json(user);
-});
-
-app.get("/get-user-by-email/:email", async (req, res) => {
-  const user = await userService.getUserByEmail(req.params.email);
-  res.status(200).json(user);
-});
-
-app.get("/get-users", async (req, res) => {
-  const users = await userService.getUsers();
-  res.status(200).json(users);
-});
-
-app.get("/delete-user-by-id/:userId", async (req, res) => {
-  await userService.deleteUserById(req.params.userId);
-  res.status(204).send();
-});
-
-app.get("/delete-user-by-email/:email", async (req, res) => {
-  await userService.deleteUserByEmail(req.params.email);
-  res.status(204).send();
-});
-
-const authService: IAuthService = new AuthService();
-
-app.get("/generate-token/:email/:password", async (req, res) => {
-  const token = await authService.generateToken(
-    req.params.email,
-    req.params.password,
-  );
-  res.status(200).json(token);
-});
-
-app.get("/is-authorized/:token/:role", async (req, res) => {
-  if (req.params.role !== "Admin" && req.params.role !== "User") {
-    res.status(400).json({ error: "Role must be either Admin or User" });
-    return;
-  }
-  const authorized = await authService.isAuthorized(
-    req.params.token,
-    req.params.role as Role,
-  );
-  res.status(200).json(authorized);
-});
-
-app.get("/renew-token/:refreshToken", async (req, res) => {
-  const token = await authService.renewToken(req.params.refreshToken);
-  res.status(200).json(token);
-});
-
-app.get("/revoke-tokens/:userId", async (req, res) => {
-  await authService.revokeTokens(req.params.userId);
-  res.status(204).send();
-});
-
-app.get("/generate-password-reset-link/:email", async (req, res) => {
-  const resetLink = await authService.generatePasswordResetLink(
-    req.params.email,
-  );
-  res.status(200).send({ resetLink });
-});
+app.use("/users", userRouter);
 
 const eraseDatabaseOnSync = false;
 sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
