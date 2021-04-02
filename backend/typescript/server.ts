83441d38ffd750233259023927bd8fc7a7b72edf
raveen-singh <emailraveensingh@gmail.com>
2021-04-02 16:34:48 -0400

    removed server changes

--- backend/typescript/server.ts
+++ backend/typescript/server.ts
@@ -3,7 +3,6 @@ import cors from "cors";
 import express from "express";
 import * as firebaseAdmin from "firebase-admin";
 
-import bodyParser from "body-parser";
 import { mongo, sequelize } from "./models";
 import authRouter from "./rest/authRoutes";
 import entityRouter from "./rest/entityRoutes";
@@ -13,8 +12,8 @@ const app = express();
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
-
 app.use(express.urlencoded());
+
 app.use("/auth", authRouter);
 app.use("/entities", entityRouter);
 app.use("/users", userRouter);
@@ -23,9 +22,9 @@ const eraseDatabaseOnSync = false;
 sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
   mongo.connect();
 
-  // firebaseAdmin.initializeApp({
-  //  credential: firebaseAdmin.credential.applicationDefault(),
-  // });
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.applicationDefault(),
+  });
 
   app.listen({ port: 5000 }, () => {
     /* eslint-disable-next-line no-console */
