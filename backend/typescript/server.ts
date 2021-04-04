dfafe35380e2cdb181d38f552fb87312aa88face
Richard Chen <40343579+chenrichard10@users.noreply.github.com>
2021-04-04 10:56:36 -0400

    GraphQL (#12)

--- backend/typescript/server.ts
+++ backend/typescript/server.ts
@@ -3,10 +3,13 @@ import cors from "cors";
 import express from "express";
 import * as firebaseAdmin from "firebase-admin";
 
+import { ApolloServer } from "apollo-server-express";
 import { mongo, sequelize } from "./models";
 import authRouter from "./rest/authRoutes";
 import entityRouter from "./rest/entityRoutes";
 import userRouter from "./rest/userRoutes";
+
+import schema from "./graphql";
 
 const app = express();
 app.use(cookieParser());
@@ -17,6 +20,10 @@ app.use(express.urlencoded());
 app.use("/auth", authRouter);
 app.use("/entities", entityRouter);
 app.use("/users", userRouter);
+
+const server = new ApolloServer({ schema });
+
+server.applyMiddleware({ app, path: "/graphql" });
 
 const eraseDatabaseOnSync = false;
 sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
