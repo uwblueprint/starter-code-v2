e4ff153419fcfced1ec725e34098dbc8f03f3087
Sherry Li <sherryhli@users.noreply.github.com>
2021-05-30 17:35:02 -0400

    Add defaultValue and allowNull options to add column example

--- backend/typescript/migrations/2021.05.30T04.47.28.add-column-example.ts
+++ backend/typescript/migrations/2021.05.30T04.47.28.add-column-example.ts
@@ -3,9 +3,11 @@ import { DataType } from "sequelize-typescript";
 import { Migration } from "../umzug";
 
 export const up: Migration = async ({ context: sequelize }) => {
-  await sequelize
-    .getQueryInterface()
-    .addColumn("test", "new", { type: DataType.BOOLEAN });
+  await sequelize.getQueryInterface().addColumn("test", "new", {
+    type: DataType.BOOLEAN,
+    defaultValue: false,
+    allowNull: false,
+  });
 };
 
 export const down: Migration = async ({ context: sequelize }) => {
