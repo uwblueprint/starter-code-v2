336b99046b0686ee1a90537a1053aa78e69ad77f
Xin Hao Zhang <xinhao.zhang98@gmail.com>
2021-06-09 19:44:16 -0400

    Create frontend sample tests

---
+++ frontend/src/components/pages/__tests__/Default.test.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Default from "../Default";
+
+// Example React test.
+// For more information on React component testing, visit:
+// https://jestjs.io/docs/tutorial-react
+// https://reactjs.org/docs/testing.html
+
+jest.mock("@apollo/client", () => ({
+  gql: () => {},
+  useMutation: () => [],
+}));
+
+describe("Default page", () => {
+  it("Should render Create Entity button", () => {
+    const page = render(<Default />);
+    const button = page.queryByText("Create Entity");
+    expect(button).toBeVisible();
+  });
+});
