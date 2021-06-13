0dad1538196d398f3c3c01f1d4fc7e18cbbc6e1e
Richard Chen <chenpipi@live.com>
2021-06-13 15:17:58 -0400

    Minor fixes

--- backend/python/app/rest/documentation_routes.py
+++ backend/python/app/rest/documentation_routes.py
@@ -1,11 +1,10 @@
 from flask import Blueprint, render_template
 
 blueprint = Blueprint(
-    "api", __name__, url_prefix="/api", template_folder="../templates"
+    "api", __name__, url_prefix="/api-docs", template_folder="../templates"
 )
 
 
-@blueprint.route("/docs", methods=["GET"], strict_slashes=False)
+@blueprint.route("/", methods=["GET"], strict_slashes=False)
 def get_docs():
-    print("sending docs")
     return render_template("swaggerui.html")
