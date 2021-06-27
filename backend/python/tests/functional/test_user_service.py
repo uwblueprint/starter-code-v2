85ca1cbf1286bb5cc9dccece869ea7012d4f1cea
Xin Hao Zhang <xinhao.zhang98@gmail.com>
2021-06-27 11:21:59 -0400

    Create test_user_service

---
+++ backend/python/tests/functional/test_user_service.py
@@ -0,0 +1,75 @@
+from flask import current_app
+import pytest
+
+from app import create_app
+from app.models.user_mg import User
+from app.services.implementations.user_service_mg import UserService as UserServiceMg
+from app.services.implementations.user_service_pg import UserService as UserServicePg
+
+'''
+Sample python test.
+For more information on pytest, visit:
+https://docs.pytest.org/en/6.2.x/reference.html
+'''
+
+
+TEST_USERS = [
+    {
+        "auth_id": "A",
+        "first_name": "Jane",
+        "last_name": "Doe",
+        "role": "Admin",
+    },
+    {
+        "auth_id": "B",
+        "first_name": "Hello",
+        "last_name": "World",
+        "role": "User",
+    }
+]
+
+
+class FirebaseUser:
+    '''
+    Mock returned firebase user
+    '''
+
+    def __init__(self):
+        self.email = "test@test.com"
+
+
+def get_expected_user(user):
+    '''
+    Remove auth_id field from user and sets email field.
+    '''
+    user["email"] = "test@test.com"
+    user.pop("auth_id", None)
+    return user
+
+
+def insert_users_mg():
+    user_instances = [User(**data) for data in TEST_USERS]
+    User.objects.insert(user_instances, load_bulk=False)
+
+
+@pytest.fixture(scope="module", autouse=True)
+def setup(module_mocker):
+    module_mocker.patch("app.services.implementations.auth_service.AuthService.is_authorized_by_role", return_value=True)
+    module_mocker.patch("firebase_admin.auth.get_user", return_value=FirebaseUser())
+
+
+@pytest.fixture
+def mg_user_service():
+    user_service = UserServiceMg(current_app.logger)
+    yield user_service
+    User.objects.delete()
+
+
+def test_get_users(mg_user_service):
+    insert_users_mg()
+    res = mg_user_service.get_users()
+    users = list(map(lambda user: user.__dict__, res))
+    users_with_email = list(map(get_expected_user, TEST_USERS))
+    for expected_user, actual_user in zip(users_with_email, users):
+        for key in users_with_email[0].keys():
+            assert expected_user[key] == actual_user[key]
