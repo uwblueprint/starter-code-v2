203ac501bf5c0bbb143af449724a6074e2e6727f
tamim-khan <tamim.m.khan@gmail.com>
2021-08-06 22:05:42 -0400

    Update validator to check for missing body

--- backend/python/app/middlewares/validate.py
+++ backend/python/app/middlewares/validate.py
@@ -27,7 +27,10 @@ def validate_request(dto_class_name):
             if request.content_type == "application/json":
                 dto = dtos[dto_class_name](**request.json)
             else:
-                req = json.loads(request.form.get("body"))
+                req_body = request.form.get("body", default=None)
+                if req_body is None:
+                    return jsonify({"error": "Missing body"}), 400
+                req = json.loads(req_body)
                 req["file"] = request.files.get("file", default=None)
                 dto = dtos[dto_class_name](**req)
             error_message = dto.validate()
