c43805f1ca77c1a36887fbd175a025a4b2d910a6
Sherry Li <sherryhli@users.noreply.github.com>
2022-01-16 15:44:18 -0500

    Fix content_type check in Python validate middleware (#153)

--- backend/python/app/middlewares/validate.py
+++ backend/python/app/middlewares/validate.py
@@ -26,7 +26,11 @@ def validate_request(dto_class_name):
     def validate_dto(api_func):
         @wraps(api_func)
         def wrapper(*args, **kwargs):
-            if request.content_type == "application/json":
+            if (
+                request.content_type
+                and len(request.content_type.split(";")) > 0
+                and request.content_type.split(";")[0] == "application/json"
+            ):
                 dto = dtos[dto_class_name](**request.json)
             else:
                 req_body = request.form.get("body", default=None)
