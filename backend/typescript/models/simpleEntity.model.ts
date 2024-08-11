4368df3270a04ca778bbff0b9535f84f63745c47
Omer Adeel <oadeel@uwaterloo.ca>
2024-08-10 22:39:00 -0400

    removed sequilize

--- backend/typescript/models/simpleEntity.model.ts
+++ backend/typescript/models/simpleEntity.model.ts
@@ -37,27 +37,3 @@ const SimpleEntitySchema: Schema = new Schema({
 export default model<SimpleEntity>("SimpleEntity", SimpleEntitySchema);
 
 // } mongodb
-// postgresql {
-import { Column, Model, Table, DataType } from "sequelize-typescript";
-
-import { Letters } from "../types";
-
-@Table({ tableName: "simple_entities" })
-export default class SimpleEntity extends Model {
-  @Column
-  string_field!: string;
-
-  @Column
-  int_field!: number;
-
-  @Column({ type: DataType.ENUM("A", "B", "C", "D") })
-  enum_field!: Letters;
-
-  @Column({ type: DataType.ARRAY(DataType.STRING) })
-  string_array_field!: string[];
-
-  @Column
-  bool_field!: boolean;
-}
-
-// } postgresql
